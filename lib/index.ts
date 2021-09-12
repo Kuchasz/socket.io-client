78d1a1dc1bc6069d0599beacc09ef8511096af6e
Rafal Pysz <kuchasz1@o2.pl>
2021-09-12 19:50:25 +0200

    changed module.exports to export

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,13 @@
-import { url } from "./url";
 import { Manager, ManagerOptions } from "./manager";
 import { Socket, SocketOptions } from "./socket";
+import { url } from "./url";
 
 const debug = require("debug")("socket.io-client");
 
 /**
- * Module exports.
- */
-
-module.exports = exports = lookup;
-
-/**
  * Managers cache.
  */
-const cache: Record<string, Manager> = (exports.managers = {});
+export const managers: Record<string, Manager> = {};
 
 /**
  * Looks up an existing `Manager` for multiplexing.
@@ -51,7 +45,7 @@ function lookup(
   const source = parsed.source;
   const id = parsed.id;
   const path = parsed.path;
-  const sameNamespace = cache[id] && path in cache[id]["nsps"];
+  const sameNamespace = managers[id] && path in managers[id]["nsps"];
   const newConnection =
     opts.forceNew ||
     opts["force new connection"] ||
@@ -64,11 +58,11 @@ function lookup(
     debug("ignoring socket cache for %s", source);
     io = new Manager(source, opts);
   } else {
-    if (!cache[id]) {
+    if (!managers[id]) {
       debug("new io instance for %s", source);
-      cache[id] = new Manager(source, opts);
+      managers[id] = new Manager(source, opts);
     }
-    io = cache[id];
+    io = managers[id];
   }
   if (parsed.query && !opts.query) {
     opts.query = parsed.queryKey;
@@ -91,7 +85,7 @@ export { protocol } from "socket.io-parser";
  * @public
  */
 
-exports.connect = lookup;
+export const connect = lookup;
 
 /**
  * Expose constructors for standalone build.
